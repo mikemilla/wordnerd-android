9b558d2b8ca670bad34f415458e544c0d7c18038
Michael Miller <mail@mikemilla.com>
2015-10-01 17:15:45 -0400

    JSON THINGS

--- app/src/main/java/com/mikemilla/wordnerd/activities/AboutActivity.java
+++ app/src/main/java/com/mikemilla/wordnerd/activities/AboutActivity.java
@@ -1,15 +1,45 @@
 package com.mikemilla.wordnerd.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.PorterDuff;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.mikemilla.wordnerd.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class AboutActivity extends Activity {
+
+    TextView textView;
+
+    // url to make request
+    private static String url = "http://api.androidhive.info/contacts/";
+
+    // JSON Node names
+    private static final String TAG_CONTACTS = "contacts";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_PHONE_MOBILE = "mobile";
+    private static final String TAG_PHONE_HOME = "home";
+    private static final String TAG_PHONE_OFFICE = "office";
+
+    // contacts JSONArray
+    JSONArray contacts = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +55,86 @@ public class AboutActivity extends Activity {
                 onBackPressed();
             }
         });
+
+        // Run and find the JSON
+        new JSONParse().execute();
+
+        textView = (TextView) findViewById(R.id.text_view);
+
+    }
+
+    private class JSONParse extends AsyncTask<String, String, JSONObject> {
+
+        private ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(AboutActivity.this);
+            pDialog.setMessage("Getting Data ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+
+            JSONParser jParser = new JSONParser();
+
+            // Getting JSON from URL
+            return jParser.getJSONFromUrl(url);
+
+        }
+        @Override
+        protected void onPostExecute(JSONObject json) {
+
+            pDialog.dismiss();
+
+            // Hashmap
+            ArrayList<HashMap<String, String>> contactList = new ArrayList<>();
+
+            try {
+                // Getting Array of Contacts
+                contacts = json.getJSONArray(TAG_CONTACTS);
+
+                // looping through All Contacts
+                for (int i = 0; i < contacts.length(); i++) {
+                    JSONObject c = contacts.getJSONObject(i);
+
+                    // Storing each json item in variable
+                    String id = c.getString(TAG_ID);
+                    String name = c.getString(TAG_NAME);
+                    String email = c.getString(TAG_EMAIL);
+                    String address = c.getString(TAG_ADDRESS);
+                    String gender = c.getString(TAG_GENDER);
+
+                    // Phone number is again JSON Object
+                    JSONObject phone = c.getJSONObject(TAG_PHONE);
+                    String mobile = phone.getString(TAG_PHONE_MOBILE);
+                    String home = phone.getString(TAG_PHONE_HOME);
+                    String office = phone.getString(TAG_PHONE_OFFICE);
+
+                    // creating new HashMap
+                    HashMap<String, String> map = new HashMap<>();
+
+                    // adding each child node to HashMap key => value
+                    map.put(TAG_ID, id);
+                    map.put(TAG_NAME, name);
+                    map.put(TAG_EMAIL, email);
+                    map.put(TAG_PHONE_MOBILE, mobile);
+
+                    // adding HashList to ArrayList
+                    contactList.add(map);
+                }
+
+                textView.setText(contactList.get(0).toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Override
